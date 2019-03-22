db347dc913890ad3cc261cac17e51edba43f4907
Your Name <you@example.com>
2019-03-22 11:29:25 -0400

    added sushi seed file

--- back-end/db/schema.rb
+++ back-end/db/schema.rb
@@ -16,46 +16,39 @@ ActiveRecord::Schema.define(version: 2019_03_21_185224) do
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "restaurant_name"
-    t.string "img_url"
-    t.string "address"
-    t.integer "avg_rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "sushi_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "rating"
-    t.text "review"
-    t.integer "bookmark_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "sushi_guides", force: :cascade do |t|
-    t.string "english_name"
-    t.string "japanese_name"
-    t.string "type"
+    t.string "type_one"
+    t.string "type_two"
+    t.string "name"
+    t.string "english"
+    t.string "kanji"
+    t.string "hiragana"
+    t.string "katakana"
     t.string "img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "location"
-    t.date "birthday"
-    t.string "username"
-    t.string "password_digest"
+    t.string "firstName"
+    t.string "lastName"
+    t.integer "birthdate"
+    t.string "userName"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
