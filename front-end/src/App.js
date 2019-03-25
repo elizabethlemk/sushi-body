6263fcde2f6d4b10a02a958109b580f9079bf683
Your Name <you@example.com>
2019-03-25 10:01:01 -0400

    sushi guides w/o cors

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import Home from './containers/Home'
+import SushiGuide from './containers/SushiGuide'
 
 class App extends Component {
+
   render() {
     return (
       <div>
       <Home />
+
+      <SushiGuide />
       </div>
     );
   }
--- front-end/src/containers/SushiGuide.js
+++ front-end/src/containers/SushiGuide.js
@@ -1,1 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Sushi from '../components/Sushi'
+
+let URL = "http://localhost:3001/sushi_guides"
+
+
+class SushiGuide extends React.Component {
+
+  state = {
+    sushis: []
+  }
+
+  componentDidMount() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(sushis => this.setState({sushis:sushis}))
+  }
+
+  render() {
+    console.log(this.state);
+    let arrayOfSushis = this.state.sushis.map(sushiObj =>
+    <Sushi key={sushiObj.id} sushi={sushiObj} />)
+    return (
+      <div>
+      {arrayOfSushis}
+      </div>
+    );
+  }
+
+}
+
+export default SushiGuide;
