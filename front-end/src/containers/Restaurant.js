6ede1aeacae42d9852e66e7b26d124f5451bfd6d
Elizabeth Le <elizabethlemk@yahoo.com>
2019-03-25 12:09:02 -0400

    merged

--- front-end/src/containers/Restaurant.js
+++ front-end/src/containers/Restaurant.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import { Grid, Container, Divider, Header, Image } from 'semantic-ui-react'
-<<<<<<< HEAD
 import MapContainer from '../components/MapContainer'
 
-=======
-import MapContainer from './MapContainer'
->>>>>>> 8ac72208c61f5c06e08c7c3e7a1aaf8fd4f3d1f7
 class Restaurant extends React.Component {
   render(){
     return(
