28f15dcb82a9982f0c26d6876f91765ab9e04eb8
Your Name <you@example.com>
2019-03-25 14:30:34 -0400

    test2

--- back-end/app/controllers/api/v1/users_controller.rb
+++ back-end/app/controllers/api/v1/users_controller.rb
@@ -5,11 +5,22 @@ class Api::V1::UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+  #
+  # def create
+  #   @user = User.create(user_params)
+  #   render json: @user
+  # end
 
   def create
-    @user = User.create(user_params)
-    render json: @user
-  end
+     @user = User.create(user_params)
+     if @user.valid?
+       render json: { user: UserSerializer.new(@user) }, status: :created
+     else
+       render json: { error: 'failed to create user' }, status: :not_acceptable
+     end
+   end
+
+
 
   def update
     @user.update(user_params)
