8232e535ffdc2e2c2376beece2f3d824fbb1d4e8
John Oncher <johnoncher@gmail.com>
2019-03-25 10:05:23 -0400

    yelp app on backend

--- back-end/app/models/user.rb
+++ back-end/app/models/user.rb
@@ -1,2 +1,31 @@
+
+require 'byebug'
+require 'rest-client'
+require '/Users/johnoncher/dev/mod-4/sushi-body/back-end/app/models/concerns/config.rb'
+
 class User < ApplicationRecord
+
+  def search_for_array_of_resturants
+    endpoint = 'https://api.yelp.com/v3/businesses/search'
+    api_key = getApiKey()
+
+    param = {
+      term: 'sushi',
+      latitude: 40.7008972,
+      longitude: -73.9876024,
+      limit: 50,
+      categories:'conveyorsushi,sushi',
+       sort_by: 'distance',
+       radius: 8000
+    }
+
+    response = RestClient.get(
+      'https://api.yelp.com/v3/businesses/search',
+       :Authorization => "bearer #{api_key}",
+       params: param
+    )
+    response = JSON.parse(response)
+    return response["businesses"]
+  end
+
 end
