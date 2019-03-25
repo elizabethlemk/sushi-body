d86a15c7cb6a20027d19478cfdfeeb55570a04cd
Elizabeth Le <elizabethlemk@yahoo.com>
2019-03-25 10:43:53 -0400

    formatted profile page

--- front-end/src/containers/User.js
+++ front-end/src/containers/User.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Grid, List} from 'semantic-ui-react'
+import { Container, Divider, Grid, Header, List, Tab} from 'semantic-ui-react'
+import Review from '../components/Review'
 
 class User extends React.Component {
+  // activeItem: is used for Semantic button toggles
   state={
     firstName: null,
     lastName: null,
@@ -13,27 +15,34 @@ class User extends React.Component {
   }
 
   componentDidMount(){
+    // uncomment this for login
     // fetch(userData).then(resp => resp.json()).then(json => {
     //   let user = json.find(data => data.id === current_user.id)
     //   this.setState({ user })
     // })
   }
 
-  render(){
-    return (
-      <div>
-        <Grid centered columns={2}>
-          <List>
-           <List.Item icon='users' content='First name Last name' />
-           <List.Item icon='marker' content='New York, NY' />
-           <List.Item icon='birthday cake' content='10/10/2010' />
-           <List.Item icon='utensils' content='Bookmarked restaurants'/>
-           <List.Item icon='newspaper outline' content='My reviews'/>
-           <List.Item icon='heart' content='Favorite items'/>
-         </List>  
-       </Grid>
-      </div>
 
+  render(){
+    // this handles the Semantic button toggles
+    const panes = [
+      { menuItem: 'Bio', render: () => <Tab.Pane><Grid columns={1} >
+        <List>
+         <List.Item icon='users' content='First name Last name' />
+         <List.Item icon='marker' content='New York, NY' />
+         <List.Item icon='birthday cake' content='10/10/2010' />
+       </List>
+     </Grid></Tab.Pane> },
+      { menuItem: 'Likes', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+      { menuItem: 'Bookmarks', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+      { menuItem: 'Reviews', render: () => <Tab.Pane><Review /></Tab.Pane> },
+    ]
+    return (
+      <Container fluid textAlign='center'>
+        <Header as='h2'>Profile</Header>
+        <Divider />
+        <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={panes} />
+        </Container>
     )
   }
 }
