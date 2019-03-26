134061473f3eec41ce8a77e5861ebf9f885312c9
Elizabeth Le <elizabethlemk@yahoo.com>
2019-03-26 16:38:25 -0400

    good

--- front-end/src/components/Signup.js
+++ front-end/src/components/Signup.js
@@ -38,31 +38,27 @@ class Signup extends React.Component{
     const stateOptions = ["California", "New York"]
     return (
       <div className="signup-form">
-      <h2>Sign up!</h2>
-      <Form>
-      <Form.Group widths='equal'>
-      <Form.Input fluid label='Username' placeholder='Username' name='username' onChange={this.handleChange}/>
-      <Form.Input fluid label='Password' placeholder='Password' name='password' onChange={this.handleChange}/>
-      <Form.Input fluid label='Password Confirmation' placeholder='Retype Password' name='password_confirmation' onChange={this.handleChange}/>
-      </Form.Group>
-      <Form.Group widths='equal'>
-      <Form.Input fluid label='First name' placeholder='First name' name='first_name' onChange={this.handleChange}/>
-      <Form.Input fluid label='Last name' placeholder='Last name' name='last_name' onChange={this.handleChange}/>
-      <DateInput
-        label='Birthday'
-        name="birthday"
-        placeholder="Date"
-        value={this.state.birthday}
-        iconPosition="left"
-        onChange={this.handleChange} />
-      <Form.Select fluid label='Location' options={stateOptions} placeholder='Location' name='location' onChange={this.handleChange}/>
-      </Form.Group>
-<<<<<<< HEAD
-      <Form.Button onClick={() => this.clickSubmit(this.state)}>Submit</Form.Button>
-=======
-      <Form.Button onClick={this.handleClick}>Submit</Form.Button>
->>>>>>> lizzy
-      </Form>
+        <h2>Sign up!</h2>
+        <Form>
+          <Form.Group widths='equal'>
+            <Form.Input fluid label='Username' placeholder='Username' name='username' onChange={this.handleChange}/>
+            <Form.Input fluid label='Password' placeholder='Password' name='password' onChange={this.handleChange}/>
+            <Form.Input fluid label='Password Confirmation' placeholder='Retype Password' name='password_confirmation' onChange={this.handleChange}/>
+          </Form.Group>
+          <Form.Group widths='equal'>
+            <Form.Input fluid label='First name' placeholder='First name' name='first_name' onChange={this.handleChange}/>
+            <Form.Input fluid label='Last name' placeholder='Last name' name='last_name' onChange={this.handleChange}/>
+            <DateInput
+              label='Birthday'
+              name="birthday"
+              placeholder="Date"
+              value={this.state.birthday}
+              iconPosition="left"
+              onChange={this.handleChange} />
+            <Form.Select fluid label='Location' options={stateOptions} placeholder='Location' name='location' onChange={this.handleChange}/>
+          </Form.Group>
+          <Form.Button onClick={() => this.clickSubmit(this.state)}>Submit</Form.Button>
+        </Form>
       </div>
     )
   }
